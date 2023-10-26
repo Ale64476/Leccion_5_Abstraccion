f17fde5c8a9ffa62cb640beea5417cbd8e2a9af6
Ale64476 <148830719+Ale64476@users.noreply.github.com>
2023-10-25 21:11:13 -0600

    cambios en la controladora, fin de la practica

--- Practica_5_Abstraccion_AJOA/src/Abstraccion/Controladora.java
+++ Practica_5_Abstraccion_AJOA/src/Abstraccion/Controladora.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class Controladora {
 
 	public static void main(String[] args) {
+		new Thread(new Reloj()).start();
+		new Thread(new Ejecutor("calc",0)).start();
 		Ejecutor e = new Ejecutor("calc", 1);
 		Scanner leer = new Scanner(System.in);
 		System.out.println("Escribe un comando para ejecutar");
