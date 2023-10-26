5aca30d1358d969679948ac0700094b46f8e5583
Ale64476 <148830719+Ale64476@users.noreply.github.com>
2023-10-25 20:18:33 -0600

    agregé una controladora

---
+++ Practica_5_Abstraccion_AJOA/src/Abstraccion/Controladora.java
@@ -0,0 +1,16 @@
+package Abstraccion;
+import java.util.Scanner;
+
+public class Controladora {
+
+	public static void main(String[] args) {
+		Ejecutor e = new Ejecutor("calc", 1);
+		Scanner leer = new Scanner(System.in);
+		System.out.println("Escribe un comando para ejecutar");
+		e.comando = leer.next();
+		if(e.necesitaEjecucion()) {
+			e.ejecutarTarea();
+		}
+	}
+
+}
